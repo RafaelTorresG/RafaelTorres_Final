e418f84cb4c256f667b2e49c32a76af6127a1476
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-23 16:23:54 +0000

    labels

--- ex1.py
+++ ex1.py
@@ -5,6 +5,8 @@ t=data1[:,0]
 x=data1[:,1]
 y=data1[:,2]
 plt.figure()
-plt.title("x vs y")
+plt.title("Posicion particula")
 plt.plot(x,y,"r")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.savefig("TorresRafael_final_15.pdf")
